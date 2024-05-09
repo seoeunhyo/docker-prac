ca0b0264fcfcd4b48f4c896e245a8e4ca07795c4
seoeunhyo <seoeunhyo99@gmail.com>
2024-05-09 11:00:48 +0900

    init project

---
+++ src/main/java/com/example/seoeunhyo/SeoeunhyoApplication.java
@@ -0,0 +1,14 @@
+package com.example.seoeunhyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoeunhyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoeunhyoApplication.class, args);
+		System.out.println("This is test text from eun!!!");
+	}
+
+}
---
+++ src/test/java/com/example/seoeunhyo/SeoeunhyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seoeunhyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoeunhyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
